f17e4377c8bd793926067252dd8ea49c38e69522
Jesus <jesusmontoya1992@yahoo.com>
2024-08-05 18:34:18 -0600

    comentario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import Axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState("");
+
+  const [predictedAge, setPredictedAge] = useState(0);
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      console.log(res.data);
+    });
+  }
+
   return (
     <div className="App">
-      <button>Predict Age</button>
+      <input 
+        placeholder='Ex. John...'
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      >
+      </input>
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>Predicted Age: {predictedAge} </h1>
     </div>
   );
 }
